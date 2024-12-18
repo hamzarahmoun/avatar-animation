77d091318774d34951bf4af3382360ee9679f3ee
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-12-18 18:05:57 +0100

    women in the center

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Experience from "./components/Experience";
 function App() {
   return (
     <>
-     <Canvas camera={{ position: [1, 1.5, 2.5], fov: 50 }} shadows>
-      <Experience/>
-    </Canvas>
+      <Canvas camera={{ position: [1, 1, 3], fov: 50 }} shadows>
+        <Experience />
+      </Canvas>
     </>
-   
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { MantineProvider } from "@mantine/core";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        globalStyles: (_theme) => ({
-          body: {
-            width: "100vw",
-            height: "100vh",
-          },
-          "#root": {
-            width: "100%",
-            height: "100%",
-          },
-        
-        }),
-      }}
-    >
+    
     <App />
-    </MantineProvider>
   </React.StrictMode>
 );
