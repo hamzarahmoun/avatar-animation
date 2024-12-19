b04b494df06014561bc7a37ee3f7119ddd76d7d3
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2024-12-19 17:27:15 +0100

    women animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
+import Interface from "./components/Interface";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Canvas camera={{ position: [1, 1, 3], fov: 50 }} shadows>
         <Experience />
       </Canvas>
+      <Interface />
     </>
   );
 }
